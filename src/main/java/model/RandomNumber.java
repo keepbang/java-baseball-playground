6b73550a2ef2ac4c03e451db039652669ae11ddc
keepbang <rlqud1125@naver.com>
2021-05-31 01:01:17 +0900

    refactor: getNumberArray 제거

--- src/main/java/model/RandomNumber.java
+++ src/main/java/model/RandomNumber.java
@@ -1,28 +1,23 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RandomNumber {
 
     private final int RANDOM_NUMBER_SIZE = 3;
 
-    private ArrayList<Integer> getNumberArray() {
-        return new ArrayList<>(Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9).collect(Collectors.toList()));
-    }
-
-    public RandomNumber() {
-    }
-
-    public ArrayList<Integer> createRandomNumbers() {
+    public List<Integer> createRandomNumbers() {
         Random random = new Random();
 
-        ArrayList<Integer> numberArray = getNumberArray();
-        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        List<Integer> numberArray = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-        while (randomNumbers.size() != RANDOM_NUMBER_SIZE) {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        while(randomNumbers.size() != RANDOM_NUMBER_SIZE) {
+
             int numberArraySize = numberArray.size();
 
             int arrayIndex = random.nextInt(numberArraySize);
