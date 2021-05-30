26f1432ef77cb2077d81f49701713ea46333316a
keepbang <rlqud1125@naver.com>
2021-05-31 02:36:48 +0900

    feature : add model
    InputView
    OutputVIew
    Baseball

--- src/main/java/model/Gamer.java
+++ src/main/java/model/Gamer.java
@@ -1,9 +1,29 @@
 package model;
 
-import java.util.ArrayList;
+import view.InputView;
+import view.OutputVIew;
+
+import java.util.List;
 
 public class Gamer {
 
+    private boolean gameState = true;
+
+    public void playGame(List<Integer> computerNumbers){
+        InputView input = new InputView();
+        OutputVIew output = new OutputVIew();
+        Baseball baseball = new Baseball();
+
+        while(gameState){
+            output.number();
+            int number = input.number();
+
+            Score score = baseball.checkNumber(computerNumbers, number);
+
+
+        }
+
+    }
 
 }
 
